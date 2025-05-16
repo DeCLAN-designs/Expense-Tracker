92bb122bea3620edffa45d553a3f92913e32a07a
Dev Simon <de.clandesigns4@gmail.com>
2025-05-16 09:18:58 +0300

    New Expense Changed

--- src/Components/NewExpense/NewExpense.jsx
+++ src/Components/NewExpense/NewExpense.jsx
@@ -24,7 +24,7 @@ const NewExpense = ({ onAddExpense }) => {
   };
 
   return (
-    <div className="new-expense">
+    <div className="new-expenses">
       <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
   );
